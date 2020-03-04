67a6e80e60978495af6df43ae0fcab8833fc2157
EC2 Default User <ec2-user@ip-172-31-43-108.ap-northeast-1.compute.internal>
2020-03-04 03:58:54 +0000

    user-edit-update-show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:index, :show, :edit, :update]
   
   def index
     @users = User.all
@@ -23,9 +24,25 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
     end
 end
